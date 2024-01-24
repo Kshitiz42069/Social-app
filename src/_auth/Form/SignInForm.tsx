f0705eea57c09763cdb866779591ebf7df27b285
KSHITIZ RAI <kshitizroy836@gmail.com>
2024-01-24 17:22:26 +0530

    test

--- src/_auth/Form/SignInForm.tsx
+++ src/_auth/Form/SignInForm.tsx
@@ -57,7 +57,7 @@ return (
 
   <Form {...form}>
     <div className="sm:w-420 flex-center flex-col">
-      <img src="/assets/images/logo.svg" alt="logo" />
+      <img src="/public/assets/images/logo.svg" alt="logo" />
       <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Log in to your Account.</h2>
       <p className="text-light-3 small-medium md:base-regular mt-2">Welcome back! please enter your details</p>
       <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
